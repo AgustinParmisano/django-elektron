825df8e10c6da0d5dce6ac58addd0041daf1a978
root <root@debian>
2017-07-22 16:59:49 -0300

    Starting Tutorial 4: Authentication & Permissions

--- tutorial/devices/urls.py
+++ tutorial/devices/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from devices import views
 
 urlpatterns = [
-    url(r'^devices/$', views.device_list),
-    url(r'^devices/(?P<pk>[0-9]+)/$', views.device_detail),
+    url(r'^devices/$', views.DeviceList.as_view()),
+    url(r'^devices/(?P<pk>[0-9]+)/$', views.DeviceDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/devices/views.py
+++ tutorial/devices/views.py
@@ -1,56 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from devices.models import Device
 from devices.serializers import DeviceSerializer
+from rest_framework import generics
 
-# Create your views here.
+class DeviceList(generics.ListCreateAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
 
-@api_view(['GET', 'POST'])
-def device_list(request, format=None):
-    """
-    List all devices, or create a new device.
-    """
-    if request.method == 'GET':
-        devices = Device.objects.all()
-        serializer = DeviceSerializer(devices, many=True)
-        return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = DeviceSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def device_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a device instance.
-    """
-    try:
-        device = Device.objects.get(pk=pk)
-    except Device.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = DeviceSerializer(device)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = DeviceSerializer(device, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        device.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class DeviceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
